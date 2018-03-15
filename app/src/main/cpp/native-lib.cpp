f0f078aeabe17fde80bbf3ec5a0a552e1075e9b3
jisuAn <ajs7270@naver.com>
2018-03-15 16:03:26 +0900

    Make Basic Directory for Everyone

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,20 @@
+#include <jni.h>
+#include <string>
+
+JNIEXPORT jstring JNICALL
+Java_cau_cse_capstone_blindaid_Camera_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return env->NewStringUTF(returnValue);
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_cau_cse_capstone_blindaid_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
