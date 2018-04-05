b5fe98b289dad9eb1a6d4e08d0f4cacca212d237
gweonyongil <kyi4210@daum.net>
2018-04-05 21:26:02 +0900

    Finish speech recoginition and use SpeechRecognition class in place of Intent for Speech

--- app/src/main/java/cau/cse/capstone/blindaid/Main/MainActivity.java
+++ app/src/main/java/cau/cse/capstone/blindaid/Main/MainActivity.java
@@ -23,14 +23,11 @@ import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
-import org.opencv.core.Rect;
-import org.opencv.core.Size;
 
 import cau.cse.capstone.blindaid.R;
 
@@ -214,5 +211,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 //resume tasks needing this permission
             }
         }
+    }
+
+    public void sayDistancefromScreen(){
+
     }
 }
