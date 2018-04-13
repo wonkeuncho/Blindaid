e2543d14af41be82180ddd9a1f64ff6af378758f
gweonyongil <kyi4210@daum.net>
2018-04-13 11:42:53 +0900

    Editing code about language debug

--- app/src/main/java/cau/cse/capstone/blindaid/Main/VoiceActivity.java
+++ app/src/main/java/cau/cse/capstone/blindaid/Main/VoiceActivity.java
@@ -101,7 +101,7 @@ public class VoiceActivity extends Activity {
         // if say yes
         if(answer.equals("yes"))
             return true;
-        // if say no or anything
+            // if say no or anything
         else
             return false;
     }
@@ -115,7 +115,7 @@ public class VoiceActivity extends Activity {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                         getString(R.string.speech_prompt));
                 sr.startListening(intent);
@@ -134,7 +134,7 @@ public class VoiceActivity extends Activity {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                         getString(R.string.speech_prompt));
                 sr.startListening(intent);
